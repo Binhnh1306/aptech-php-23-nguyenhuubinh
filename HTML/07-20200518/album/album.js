49ade246d4be04c2e4d3fb06f9adcdb23d93b45c
binhnh1306 <binhnh1306@gmail.com>
2020-06-05 19:27:25 +0700

    edited Album

---
+++ HTML/07-20200518/album/album.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $("#phanTren").toggle(300);
+    });
+});
+
+// $(document).ready(function(){
+
+//     $("#nutAn").click(function(){
+//       $("#canAn").toggle(800);
+//     });
+  
+//   //   $("#nutAn").click(function(){
+//   //     $("#canAn").hide();
+//   //   });
+  
+//   });
+  
+  
